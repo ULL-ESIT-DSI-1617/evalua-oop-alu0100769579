c27fbe5479e4f97a062fcca79f2ae65037d9e30a
Raul Martin <alu0100769579@ull.edu.es>
2017-04-04 20:30:23 +0000

    Reescribiendo las clases usando ECMA6

--- rtextcell.js
+++ rtextcell.js
@@ -1,16 +1,29 @@
-var TextCell = require('./textcell.js')
+"use strict";
 
-function RTextCell(text) {
-  TextCell.call(this, text);
-}
-RTextCell.prototype = Object.create(TextCell.prototype);
-RTextCell.prototype.draw = function(width, height) {
-  var result = [];
-  for (var i = 0; i < height; i++) {
-    var line = this.text[i] || "";
-    result.push(" ".repeat(width - line.length) + line);
+var TextCell = require("./textcell.js");
+
+
+// RTextCell Class
+
+class RTextCell extends TextCell {
+  
+  constructor(text) {
+	  
+	  super(text);
   }
-  return result;
-};
+  
+  draw(width, height) {
+    
+    let result = [];
+	  for (let i = 0; i < height; i++) {
+	    let line = this.text[i] || "";
+	    result.push(" ".repeat(width - line.length) + line);
+	  }
+	  return result;
+  }
+}
+
+// End RTextCell
+
 
 module.exports = RTextCell;
